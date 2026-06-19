de9ef356cd72f1e0c76d999cb8c5b7fc19ce80f9
aaneleh <helenakurzgon@gmail.com>
2026-06-19 19:35:29 -0300

    arquivos de build

---
+++ build/CMakeFiles/Keyframes.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Keyframes.dir/common/glad.c.obj"
+  "CMakeFiles/Keyframes.dir/common/glad.c.obj.d"
+  "CMakeFiles/Keyframes.dir/src/Keyframes.cpp.obj"
+  "CMakeFiles/Keyframes.dir/src/Keyframes.cpp.obj.d"
+  "Keyframes.exe"
+  "Keyframes.exe.manifest"
+  "Keyframes.pdb"
+  "libKeyframes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Keyframes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
